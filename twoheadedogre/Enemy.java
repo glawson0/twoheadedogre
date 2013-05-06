77cb22da3d62d5c72ba07db9c173863301dfd1c6
Geoffrey Lawson <geoffrey.lawson0@gmail.com>
2013-05-06 00:38:14 -0700

    ogre moves and spins
    DON'T USE KNIGHT currently broken

---
+++ twoheadedogre/Enemy.java
@@ -0,0 +1,28 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+
+/**
+ * Write a description of class Enemy here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Enemy extends Actor
+{
+    /**
+     * Act - do whatever the Enemy wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        checkCollision();
+    }
+    protected void checkCollision(){
+        java.util.List <Actor> objs= getIntersectingObjects(Actor.class);
+        for(Actor obj: objs){
+            if (obj instanceof Ogre){
+                getWorld().removeObject(this);
+            }
+        }
+    }
+}
---
+++ twoheadedogre/Knight.java
@@ -0,0 +1,39 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Knight here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Knight extends Enemy
+{
+    /**
+     * Act - do whatever the Knight wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    private Ogre ogre;
+    public Knight(){
+        super();
+        ogre=(Ogre) (getWorld().getObjects(Ogre.class)).get(0);
+    }
+    public void act() 
+    {
+        checkCollision();
+    }
+    private void move(){
+        int x=0;
+        int y=0;
+        if (ogre.getX()< getX() ){
+            x-=3;
+        }else if (ogre.getX()> getX() ){
+            x+=3;
+        }
+        if (ogre.getY()< getY() ){
+            y-=3;
+        }else if (ogre.getY()> getY() ){
+            y+=3;
+        }
+        setLocation(getX()+x,getY()+y);
+    }
+}
