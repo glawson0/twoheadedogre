109769a3add757bb75d9fe1eb55940373cb5c683
glawson0 <geoffrey.lawson0@gmail.com>
2013-05-07 17:12:52 -0700

    working on world

--- twoheadedogre/GrassWorld.java
+++ twoheadedogre/GrassWorld.java
@@ -1,5 +1,6 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.util.LinkedList;
+import java.util.Random;
 
 /**
  * Write a description of class GrassWorld here.
@@ -16,10 +17,13 @@ public class GrassWorld extends World
      */
     public Ogre ogre;
     public Face face;
+    private Random r;
+    private LinkedList<Enemy> baddies;
     public GrassWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(700, 600, 1);
+        r=new Random();
         face = new Face(true);
         ogre = new Ogre(new ImagePackage(), face);
         populate();
@@ -28,6 +32,22 @@ public class GrassWorld extends World
     public void populate() {
         addObject(face,50,50);
         addObject(ogre, 350, 300);
-        addObject(new Knight(new ImagePackage(), ogre), 500,600);
+        baddies= makeEnemies(5);
+        
+        for(Enemy E: baddies){
+            switch(r.getInt(4)){
+                case 0: addObject(E,0
+                
+            }
+        }
+        
+    }
+    
+    private LinkedList<Enemy> makeEnemies(int numE){
+        LinkedList<Enemy> enemies= new LinkedList<Enemy>();
+        for (int i=0; i< numE; i++){
+            enemies.add(new Knight(new ImagePackage(), ogre));
+        }
+        return enemies
     }
 }
