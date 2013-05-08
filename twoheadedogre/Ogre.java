402bceb15a4462446b04e039fbc0a20441516ce7
Geoffrey Lawson <geoffrey.lawson0@gmail.com>
2013-05-08 01:56:20 -0700

    added ogre AI

--- twoheadedogre/Ogre.java
+++ twoheadedogre/Ogre.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.List;
 /**
  * Write a description of class Ogre here.
  * 
@@ -37,7 +37,76 @@ public class Ogre extends SoundActor
             switchWait--;
         }
         procInput();
-    }    
+        procAI();
+    }
+    private void procAI (){
+        List<Enemy> enemies = getWorld().getObjects(Enemy.class);
+        Enemy closest=null;
+        int x1=0,x2=0,y1=0,y2=0;
+        for (Enemy E: enemies){
+            if(closest==null){
+                closest=E;
+                x2=getX()-closest.getX();
+                y2=getY()-closest.getY();
+            }
+            else{
+                x1=getX()-E.getX();
+                y1=getY()-E.getY();
+                x2=getX()-closest.getX();
+                y2=getY()-closest.getY();
+                if((x1*x1)+(y1*y1)< (x2*x2)+(y2+y2)){
+                    closest=E;
+                    x2=getX()-closest.getX();
+                    y2=getY()-closest.getY();
+                }
+            }
+        }
+        
+        if (closest==null){
+            return;
+        }
+        
+        if(walking){
+            int dir;
+            if((x2*x2) <(y2*y2)){
+                if(x2>0){
+                    dir=WEST;
+                }else{
+                    dir=EAST;
+                }
+            }else{
+                if(y2>0){
+                    dir=NORTH;
+                }else{
+                    dir=SOUTH;
+                }
+            }
+            switch(dir){
+            case 0: setRotation(0);
+                    break;
+            case 1: setRotation(270);
+                    break;
+            case 2: setRotation(180);
+                    break;
+            case 3: setRotation(90);
+                    break;
+            }
+        }else{
+            int x=0;
+            int y=0;
+            if(x2>0){
+                x-=5;
+            }else if(x2<0){
+                x+=5;
+            }
+            if(y2>0){
+                y-=5;
+            }else if(y2<0){
+                y+=5;
+            }
+            setLocation(getX()+x,getY()+y);
+        }
+    }
     private void switchHead(){
         if (Greenfoot.isKeyDown("s")){
             walking= !walking;
