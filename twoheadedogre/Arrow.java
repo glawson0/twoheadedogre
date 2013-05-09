4d550f793c621401b64b88d0dc1a659db356e2dd
Robin Verweij <robinverweij@hotmail.com>
2013-05-09 00:55:46 -0700

    changed some dialog class stuff

--- twoheadedogre/Arrow.java
+++ twoheadedogre/Arrow.java
@@ -7,12 +7,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @version (a version number or a date)
  */
 public class Arrow extends Actor
-{
-    /**
-     * Act - do whatever the Arrow wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    
+{    
     public void act() 
     {
         move(2);
--- twoheadedogre/DialogPackage.java
+++ twoheadedogre/DialogPackage.java
@@ -1,8 +1,8 @@
 /**
  * Dialog package is for storyline dialog.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Robin Verweij
+ * @version super beta
  */
 public class DialogPackage  
 {
@@ -13,24 +13,38 @@ public class DialogPackage
      */
     private boolean dialogPlaying;
     
-    /**
-     * Constructor for objects of class DialogPackage
-     */
     public DialogPackage()
     {
         dialogPlaying = false;
     }
     
-    
-    public void playDialog(String classType) {
+    /**
+     * Must pass classType as a string
+     *  ex: playDialog("knight",face); for a knight spawning with dialog.
+     * Gordo is the face variable, telling us which ogre is being controlled at the moment.
+     *  This allows us to switch between dialogs if we so wish.
+     */
+    public void playDialog(String classType, boolean gordo) {
         if (!dialogPlaying) {
             if (classType.equals("ogre")) {
                 // randomly generate a number
                 // set dialogPlaying to true
                 // play a dialog corresponding to that number
                 // set dialogPlaying to false
+                if (gordo) {
+                    // Gordo (stuuuupid)
+                }
+                else {
+                    // Omak (smart.)
+                }
             }
             else if (classType.equals("knight")) {
+                // randomly generate a number
+                // set dialogPlaying to true
+                // play a dialog corresponding to that number
+                // set dialogPlaying to false
+            }
+            else if (classType.equals("archer")) {
                 // randomly generate a number
                 // set dialogPlaying to true
                 // play a dialog corresponding to that number
